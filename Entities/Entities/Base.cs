e716c4d6d98274194acc294455a83824978f598a
itallo.php <itallo.php@gmail.com>
2020-09-03 00:24:53 -0300

    create entity base class

---
+++ Entities/Entities/Base.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Notifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.Entities
+{
+    public class Base: Notifies
+    {
+        [Display(Name="Codigo")]
+        public int Id { get; set; }
+        
+        [Display(Name="Nome")]
+        public string Name { get; set; }
+    }
+}
