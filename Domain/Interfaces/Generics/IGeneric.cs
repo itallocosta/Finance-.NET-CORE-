b25cd6f94118fc89f70e1fe8280d298d7dca7668
itallo.php <itallo.php@gmail.com>
2020-09-20 01:13:23 -0300

    Create generic interface repository

---
+++ Domain/Interfaces/Generics/IGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Generics
+{
+    public interface IGeneric<T> where T : class
+    {
+        Task Add(T objeto);
+        Task Update(T objeto);
+        Task Delete(T objeto);
+        Task<T> GetEntityById(int id);
+        Task<IList<T>> List();
+    }
+}
