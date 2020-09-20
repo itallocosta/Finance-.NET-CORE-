c2738cdfd7d557a40a403c20dbdc7f3717c8366b
itallo.php <itallo.php@gmail.com>
2020-09-20 06:12:04 -0300

    Implement class Context and Generic Repository

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,35 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options): base(options)
+        {
+        }
+
+        public DbSet<BankAccount> BankAccount { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<SubCategory> SubCategory { get; set; }
+    
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(GetStringConnection());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+        
+        private string GetStringConnection()
+        {
+            var stringConexao = "Data Source=DESKTOP-2CIR9JE\\SQLEXPRESS;Initial Catalog=Finance;Integrated Security=True";
+            return stringConexao;
+        }
+        
+    }
+}
---
+++ Infrastructure/Repository/Generics/GenericRepository.cs
@@ -0,0 +1,84 @@
+﻿using Domain.Interfaces.Generics;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class GenericRepository<T> : IGeneric<T>, IDisposable where T : class
+    {
+
+        // To detect redundant calls
+        private bool _disposed = false;
+
+        // Instantiate a SafeHandle instance.
+        private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
+
+        private readonly DbContextOptions<ContextBase> _optionBuilder;
+
+        public GenericRepository()
+        {
+            _optionBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task Add(T objeto)
+        {
+            using var data = new ContextBase(_optionBuilder);
+            await data.Set<T>().AddAsync(objeto);
+            await data.SaveChangesAsync();
+        }
+
+        public async Task Delete(T objeto)
+        {
+            using var data = new ContextBase(_optionBuilder);
+            data.Set<T>().Remove(objeto);
+            await data.SaveChangesAsync();
+        }
+
+        public async Task<T> GetEntityById(int id)
+        {
+            using var data = new ContextBase(_optionBuilder);
+            return await data.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> List()
+        {
+            using var data = new ContextBase(_optionBuilder);
+            return await data.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task Update(T objeto)
+        {
+            using var data = new ContextBase(_optionBuilder);
+            data.Set<T>().Update(objeto);
+            await data.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _safeHandle?.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+}
